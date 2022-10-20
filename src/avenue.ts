6fabca835ddecf0b4b6a35949573a827ab28bfff
brunobarreirazup <bruno.barreira@zup.com.br>
2022-10-20 17:33:09 -0300

    refactor: remove unused await

--- src/avenue.ts
+++ src/avenue.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { PATH } from "./path"
-import { PROPS } from "./constant"
+import { AVENUECONFIGS } from "./constant"
 import { 
     AuthorizeAppRequest, 
     AuthorizeAppResponse, 
@@ -15,33 +15,33 @@ import {
 export class AvenueRequest {
     
     async authorizeApp(user_email: string): Promise<AuthorizeAppResponse> {
-        return await axios.post(
-            `${PATH.AUTHORIZE_APP}?key=${PROPS.API_KEY}`,
+        return axios.post(
+            `${PATH.AUTHORIZE_APP}?key=${AVENUECONFIGS.API_KEY}`,
             new AuthorizeAppRequest(user_email)
         ).then(result => result.data)
         .catch(error => console.log(error))
     }
 
     async authorizationStatus(code: string): Promise<AuthorizeAppStatusResponse> {
-        return await axios.post(
-            `${PATH.AUTHORIZATION_STATUS}?key=${PROPS.API_KEY}`,
+        return axios.post(
+            `${PATH.AUTHORIZATION_STATUS}?key=${AVENUECONFIGS.API_KEY}`,
             new CodeRequest(code)
         ).then(result => result.data)
         .catch(error => console.log(error))
     }
 
     async authTokens(code: string): Promise<TokensResponse> {
-        return await axios.post(
-            `${PATH.AUTH_TOKENS}?key=${PROPS.API_KEY}`,
+        return axios.post(
+            `${PATH.AUTH_TOKENS}?key=${AVENUECONFIGS.API_KEY}`,
             new CodeRequest(code)
         ).then(result => result.data)
         .catch(error => console.log(error))
     }
 
     async getPublicKey(): Promise<GetPublicKeyResponse> {
-        return await axios.get(
+        return axios.get(
             PATH.GET_PUBLIC_KEY,
-            { params: { key: PROPS.API_KEY }}
+            { params: { key: AVENUECONFIGS.API_KEY }}
         ).then(result => result.data)
         .catch(error => console.log(error))
     }
@@ -51,24 +51,24 @@ export class AvenueRequest {
         access_token: string, 
         refresh_token: string
     ): Promise<TokensResponse> {
-        return await axios.post(
-            `${PATH.REFRESH_TOKENS}?key=${PROPS.API_KEY}`,
+        return axios.post(
+            `${PATH.REFRESH_TOKENS}?key=${AVENUECONFIGS.API_KEY}`,
             new RefreshTokensRequest(access_token, refresh_token, code)
         ).then(result => result.data)
         .catch(error => console.log(error))
     }
 
     async getQuotes(access_token: string): Promise<QuotesResponse> {
-        return await axios.get(
-            `${PATH.GET_QUOTES}?key=${PROPS.API_KEY}`,
+        return axios.get(
+            `${PATH.GET_QUOTES}?key=${AVENUECONFIGS.API_KEY}`,
             { headers: { "Authorization": `Bearer ${access_token}` } }
         ).then(result => result.data)
         .catch(error => console.log(error))
     }
 
     async getPositionAssets(access_token: string): Promise<QuotesResponse> {
-        return await axios.get(
-            `${PATH.GET_BALANCE_POSITION}?key=${PROPS.API_KEY}`,
+        return axios.get(
+            `${PATH.GET_BALANCE_POSITION}?key=${AVENUECONFIGS.API_KEY}`,
             { headers: { "Authorization": `Bearer ${access_token}` } }
         ).then(result => result.data)
         .catch(error => console.log(error))
