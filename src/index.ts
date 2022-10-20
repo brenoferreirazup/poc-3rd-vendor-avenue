de85950a653940f3af64ebc15564a0bd13351d99
brunobarreirazup <bruno.barreira@zup.com.br>
2022-10-20 17:31:29 -0300

    refactor: remove environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,34 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import { AvenueRequest } from "./avenue"
 import { PATH } from './path'
+import { AVENUECONFIGS } from "./constant"
 
+const sout = (path: string, response: any) => {
+    console.log('[AVENUE RESPONSE]');
+    console.log(`${path}`);
+    console.log(response, '\n');
+}
 
 (async function(){
     let response = {}
     const avenue = new AvenueRequest()
 
-    // nyzdlinxxym3tqfk42skgj@avenue.us
-    // rtgmvgrjsfhxxjlxjb7ogm@avenue.us
-    // sbdsjrm27nialvsobfctza@avenue.us
-    const authorizeAppResponse = await avenue.authorizeApp("nyzdlinxxym3tqfk42skgj@avenue.us")
-    console.log(`${PATH.AUTHORIZE_APP}`, authorizeAppResponse)
+    const authorizeAppResponse = await avenue.authorizeApp(AVENUECONFIGS.CLIENT_EMAIL)
+    sout(PATH.AUTHORIZE_APP, authorizeAppResponse)
 
     response = await avenue.authorizationStatus(authorizeAppResponse.code)
-    console.log(`${PATH.AUTHORIZATION_STATUS}`, response)
+    sout(PATH.AUTHORIZATION_STATUS, response)
 
     const tokenResponse = await avenue.authTokens(authorizeAppResponse.code)
-    console.log(`${PATH.AUTH_TOKENS}`, tokenResponse)
+    sout(PATH.AUTH_TOKENS, tokenResponse)
 
     response = await avenue.getQuotes(tokenResponse.access_token)
-    console.log(`${PATH.GET_QUOTES}`, response)
+    sout(PATH.GET_QUOTES, response)
 
     response = await avenue.getPositionAssets(tokenResponse.access_token)
-    console.log(`${PATH.GET_BALANCE_POSITION}`, response)
+    sout(PATH.GET_BALANCE_POSITION, response)
 
     response = await avenue.refreshTokens(authorizeAppResponse.code, tokenResponse.access_token, tokenResponse.refresh_token)
-    console.log(`${PATH.REFRESH_TOKENS}`, response)
+    sout(PATH.REFRESH_TOKENS, response)
 })()
