bce835d9e19608d3a7bfaa34b1f4c1a8bec7af5c
brunobarreirazup <bruno.barreira@zup.com.br>
2022-10-20 17:39:37 -0300

    refactor: separate terminal execution from web execution

---
+++ src/terminal.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import { AvenueRequest } from "./avenue"
+import { PATH } from './path'
+import { AVENUECONFIGS } from "./constant"
+
+const sout = (path: string, response: any) => {
+    console.log('[AVENUE RESPONSE]');
+    console.log(`${path}`);
+    console.log(response, '\n');
+}
+
+(async function(){
+    let response = {}
+    const avenue = new AvenueRequest()
+
+    const authorizeAppResponse = await avenue.authorizeApp(AVENUECONFIGS.CLIENT_EMAIL)
+    sout(PATH.AUTHORIZE_APP, authorizeAppResponse)
+
+    response = await avenue.authorizationStatus(authorizeAppResponse.code)
+    sout(PATH.AUTHORIZATION_STATUS, response)
+
+    const tokenResponse = await avenue.authTokens(authorizeAppResponse.code)
+    sout(PATH.AUTH_TOKENS, tokenResponse)
+
+    response = await avenue.getQuotes(tokenResponse.access_token)
+    sout(PATH.GET_QUOTES, response)
+
+    response = await avenue.getPositionAssets(tokenResponse.access_token)
+    sout(PATH.GET_BALANCE_POSITION, response)
+
+    response = await avenue.refreshTokens(authorizeAppResponse.code, tokenResponse.access_token, tokenResponse.refresh_token)
+    sout(PATH.REFRESH_TOKENS, response)
+})()
